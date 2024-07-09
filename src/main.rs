15b2d266da0d0ed5046503580a74417956eab142
martcpp <mordecaiemmanueletukudo@gmail.com>
2024-07-09 15:30:53 +0100

    starting

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
+
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+   }
+
+   #[derive(Debug)]
+   struct QuestionId(String);
+
+   
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+    match id.is_empty() {
+    false => Ok(QuestionId(id.to_string())),
+    true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+    }
+    }
+   }
+
+   impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) ->
+Self {
+    Question {
+    id,
+    title,
+    content,
+    tags,
+    }
+    }
+   }
+   fn main() {
+    let question = Question::new(
+    QuestionId::from_str("1").expect("Invalid Question"),
+    "First Question".to_string(),
+    "Content of question".to_string(),
+    Some(vec!["faq".to_string()]),
+    );
+    println!("{:#?}", question);
+   } 
